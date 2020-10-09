7df25b018d50088103874079b854f1a2a610a68a
Jeffe <j.garner200@gmail.com>
2020-10-09 17:00:09 -0400

    small tweaks, version 1.1

--- main.js
+++ main.js
@@ -1,53 +1,62 @@
-$(document).ready(function () {
-  progressShipping = $("ul li:first-of-type");
-  progresspayment = $("ul li:last-of-type");
-  shippingForm = $("#shipping");
-  confirmshippingBtn = $("#next");
-  paymentForm = $("#payment");
-  reviseShippingBtn = $("#revise");
-  confirmPaymentBtn = $("#done");
-  thankYou = $("#thankyou");
-  redoBtn = $("#thankyou button");
+progressShipping = $("ul li:first-of-type");
+progresspayment = $("ul li:last-of-type");
+shippingForm = $("#shipping");
+confirmshippingBtn = $("#next");
+paymentForm = $("#payment");
+reviseShippingBtn = $("#revise");
+confirmPaymentBtn = $("#done");
+thankYou = $("#thankyou");
+redoBtn = $("#thankyou button");
 
-  // To start, the 'shipping' part of the progress bar is active (green)
-  progressShipping.addClass("active");
+// To start, the 'shipping' part of the progress bar is active (green)
+progressShipping.addClass("active");
 
-  // When the confirm shipping button is pressed, the shipping form shrinks, fades, and has no display. The second form comes in from the right with animation. the progress bar is altered
-  confirmshippingBtn.click(function (e) {
-    e.preventDefault();
-    shippingForm.hide();
-    paymentForm.show();
-    paymentForm.addClass("slide-in-right");
-    progresspayment.addClass("active");
-    setTimeout(() => {
-      paymentForm.removeClass("slide-in-right");
-      shippingForm.removeClass("scale-out");
-    }, 500);
-  });
+// When the confirm shipping button is pressed, the shipping form shrinks, fades, and has no display. The second form comes in from the right with animation. the progress bar is altered
+confirmshippingBtn.click(function (e) {
+  e.preventDefault();
+  shippingForm.hide();
+  paymentForm.show();
+  paymentForm.addClass("slide-in-right");
+  progresspayment.addClass("active");
+  setTimeout(() => {
+    paymentForm.removeClass("slide-in-right");
+    shippingForm.removeClass("scale-out");
+  }, 510);
+});
 
-  // when we click the revise shipping button, the payment form shrinks, fades, and has no display. The first form comes in from the left. the progress bar is altered
-  reviseShippingBtn.click(function (e) {
-    e.preventDefault();
-    paymentForm.hide();
-    shippingForm.show();
-    shippingForm.addClass("slide-in-left");
-    progresspayment.removeClass("active");
-    setTimeout(() => {
-      paymentForm.removeClass("scale-out");
-      shippingForm.removeClass("slide-in-left");
-    }, 500);
-  });
+// when we click the revise shipping button, the payment form shrinks, fades, and has no display. The first form comes in from the left. the progress bar is altered
+reviseShippingBtn.click(function (e) {
+  e.preventDefault();
+  paymentForm.hide();
+  shippingForm.show();
+  shippingForm.addClass("slide-in-left");
+  progresspayment.removeClass("active");
+  setTimeout(() => {
+    paymentForm.removeClass("scale-out");
+    shippingForm.removeClass("slide-in-left");
+  }, 510);
+});
 
-  // when we click the confirm payment button,
+// when we click the confirm payment button,
 
-  confirmPaymentBtn.click(function (e) {
-    e.preventDefault();
-    paymentForm.hide();
-    thankYou.show();
-    thankYou.addClass("slide-up");
-    setTimeout(() => {
-      redoBtn.show();
-      redoBtn.addClass("appear");
-    }, 500);
-  });
+confirmPaymentBtn.click(function (e) {
+  e.preventDefault();
+  paymentForm.hide();
+  thankYou.show();
+  thankYou.addClass("slide-up");
+  setTimeout(() => {
+    redoBtn.show();
+    redoBtn.addClass("appear");
+    thankYou.removeClass("slide-up");
+  }, 1000);
+  setTimeout(() => {
+    redoBtn.removeClass("appear");
+  }, 2000);
+});
+
+redoBtn.click(function (e) {
+  e.preventDefault();
+  thankYou.hide();
+  redoBtn.hide();
+  shippingForm.show();
 });
